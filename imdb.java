e23a51678c32c8844e241e0af46d1ccde11dcfbf
Rohithabbunandurkar <105877900+Rohithabbunandurkar@users.noreply.github.com>
2022-10-14 13:33:18 +0530

    Update imdb.java

--- imdb.java
+++ imdb.java
@@ -1,16 +1,4 @@
-package CodingTest;
 
-import static org.testng.Assert.assertEquals;
-
-import java.time.Duration;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
-import org.testng.annotations.Test;
 
 public class imdb {
 	@Test
